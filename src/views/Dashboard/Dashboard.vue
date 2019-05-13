31f4d4adac1265995b18b644f69ff71c6ae716e5
jovianri <jovianri@hotmail.com>
2019-05-13 10:18:26 +0200

    segundo commit

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,45 +1,59 @@
 <template>
-    <div class="dashboard">
+  <div>
+    <div v-if="(logeado && usuario.admin)">
+      <div class="dashboard">
         <h1 class="dashboard__title">Panel de Control</h1>
         <hr>
         <button class="dashboard__item" v-for="nombreTabla in tablas" v-bind:key="nombreTabla">
-            <router-link class="btn btn-default dashboard__link" :to="{name: 'databaseTable', params: {tabla: nombreTabla}}">{{ nombreTabla }}</router-link>
+          <router-link
+            class="btn btn-default dashboard__link"
+            :to="{name: 'databaseTable', params: {tabla: nombreTabla}}"
+          >{{ nombreTabla }}</router-link>
         </button>
         <hr>
         <transition name="fade" mode="out-in">
-            <router-view></router-view>
+          <router-view></router-view>
         </transition>
+      </div>
     </div>
+    <div v-else>
+      <h2>No tienes permiso para entrar en esta página</h2>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
-
-    computed:{
-        tablas():any{
-            return ['ciudades', 'locales', 'usuarios' ,'categorias', 'pedidos']
-        },
-
-        table(this:any):any{
-            return this.$store.getters.tableSearched;
-        }
+  computed: {
+    usuario(): any {
+      return this.$store.getters.getUser;
+    },
+    logeado(): any {
+      return this.$store.getters.logeado;
     },
 
-    methods:{
-        mostrarTabla(tabla:any){
-            this.$store.dispatch('changeTableSearched', tabla);
-        },
+    tablas(): any {
+      return ["ciudades", "locales", "usuarios", "categorias", "pedidos"];
+    },
 
+    table(this: any): any {
+      return this.$store.getters.tableSearched;
     }
-})
+  },
+
+  methods: {
+    mostrarTabla(tabla: any) {
+      this.$store.dispatch("changeTableSearched", tabla);
+    }
+  }
+});
 </script>
 
 
 <style lang="scss">
-
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity .1s
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s;
+}
 </style>
