a175eae147e75fa387ebe5bb769e6d5aaec09a4f
jovianri <jovianri@hotmail.com>
2019-05-17 10:05:38 +0200

    Corrección de errores

--- src/components/CarritoDesplegable.vue
+++ src/components/CarritoDesplegable.vue
@@ -1,106 +1,83 @@
 <template>
-
-<div class="container">
-
-  <div @click="changeVisibility">
-    <img src="../icons/shopping_cart.png" alt="Shoppingcart">
-  </div>
-
-  <transition name="fade">
-    <div class="shopping-cart" v-if="visibility">
-
-      <div class="shopping-cart-header">
-        <span class="badge">&nbsp;</span>
-        <div class="shopping-cart-total">
-          <span class="lighter-text">Total </span>
-          <span class="main-color-text">{{ carrito.total }}€</span>
-        </div>
-      </div>
-
-      <ul class="shopping-cart-items">
-
-        <li class="clearfix" v-for="escogido in articulosEscogidos" :key="escogido.id">
-          <!-- <img :src="'/img/'+escogido.local.nombreImg"/> -->
-          <span class="item-name">{{ escogido.local.nombre }}</span>
-          <span class="item-price">{{ escogido.local.precio * escogido.cantidad }}€</span>
-          <span class="item-quantity">{{ escogido.cantidad }} uds.</span>
-
-          <div class="item__buttons">
-            <span class="item-add" @click="addProduct(escogido.local)">
-              <img src="../icons/add_icon.png" alt="">
-            </span>
-            <span class="item-delete" @click="deleteProduct(escogido.local)">
-              <img src="../icons/quit_icon.png" alt="">
-            </span>
-          </div>
-          
-        </li>
-
-      </ul>
-
-      <div v-if="logeado == 1">
-        <a class="button" @click="comprar">Comprar</a> <!-- onclick -->
-      </div>
-      <div v-else>
-        <div class="button" data-toggle="modal" data-target="#login"> Login</div>
-      </div>
-
+  <div class="container">
+    <div @click="changeVisibility">
+      <img src="../icons/shopping_cart.png" alt="Shoppingcart">
     </div>
-  </transition>
-  
 
-</div>
+    <transition name="fade">
+      <div class="shopping-cart" v-if="visibility">
+        <div class="shopping-cart-header">
+          <span class="badge">&nbsp;</span>
+          <div class="shopping-cart-total">
+            <span class="lighter-text">Total</span>
+            <span class="main-color-text">{{ carrito.total }}€</span>
+          </div>
+        </div>
+
+        <ul class="shopping-cart-items">
+          <li class="clearfix" v-for="escogido in articulosEscogidos" :key="escogido.id">
+            <!-- <img :src="'/img/'+escogido.local.nombreImg"/> -->
+            <span class="item-name">{{ escogido.local.nombre }}</span>
+            <span class="item-price">{{ escogido.local.precio * escogido.cantidad }}€</span>
+            <span class="item-quantity">{{ escogido.cantidad }} uds.</span>
+
+            <div class="item__buttons">
+              <span class="item-add" @click="addProduct(escogido.local)">
+                <img src="../icons/add_icon.png" alt>
+              </span>
+              <span class="item-delete" @click="deleteProduct(escogido.local)">
+                <img src="../icons/quit_icon.png" alt>
+              </span>
+            </div>
+          </li>
+        </ul>
+        <a class="button" @click="comprar">Comprar</a>
+        <!-- onclick -->
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
+  mounted() {},
 
-    mounted(){
-
+  methods: {
+    addProduct(this: any, local: any) {
+      this.$store.dispatch("anyadirCarrito", local);
     },
 
-    methods:{
-
-      addProduct(this:any, local:any){
-        this.$store.dispatch('anyadirCarrito', local);
-      },
-
-      deleteProduct(local:any){
-        this.$store.dispatch('deleteOfCart',local);
-      },
-
-      changeVisibility(){      
-        this.$store.dispatch('cambiarEstadoCarrito', !this.visibility);
-      },
-
-      comprar(){
-        this.$store.dispatch('comprar');
-      }
-
+    deleteProduct(local: any) {
+      this.$store.dispatch("deleteOfCart", local);
     },
 
-    computed:{
+    changeVisibility() {
+      this.$store.dispatch("cambiarEstadoCarrito", !this.visibility);
+    },
 
-      articulosEscogidos():any{
-        return this.$store.getters.dameCarrito;
-      },
-
-      carrito():any{
-        return this.$store.state.escogidosCarrito;
-      },
-
-      visibility():any{
-        return this.$store.getters.estadoCarrito;
-      }
-
+    comprar() {
+      this.$store.dispatch("comprar");
     }
+  },
 
-})
+  computed: {
+    articulosEscogidos(): any {
+      return this.$store.getters.dameCarrito;
+    },
+
+    carrito(): any {
+      return this.$store.state.escogidosCarrito;
+    },
+
+    visibility(): any {
+      return this.$store.getters.estadoCarrito;
+    }
+  }
+});
 </script>
 
 
 <style lang="scss" scoped>
-    @import "../styles/style.scss";
-
+@import "../styles/style.scss";
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,137 +1,168 @@
 <template>
+  <div class="menu">
+    <FormLogSign/>
 
-    <div class="menu">
-
-        <FormLogSign/>
-
-        <div class="menu__item menu__item--logo">
-            <router-link to="/">
-                <img src="../images/logo.png" alt="Localia Home" style="width:70px;">
-            </router-link>
-        </div>
-
-        <div class="menu__item menu__item--default">
-            <form class="form" v-on:submit.prevent="buscar">
-                <i class="fas fa-search"></i>
-                <input type="text" placeholder="Lugares.." name="search" maxlength="50" id="ciudadBuscar" class="menu__search"><!-- v-model="ciudad" -->
-                <router-link class="form__button" to="/search" tag="input" type="submit" value="Buscar" >Buscar</router-link>
-            </form>
-        </div>
-        <div class="menu__item menu__item--content">
-            <div @click="doDropdown">
-                <img src="../icons/burger_menu.png" alt="">
-            </div>
-        </div>
-
-        <transition name="fade">
-        <div class="menu__item menu__item--layout">
-            <div class="submenu-danger" id="submenuDanger">
-                <div class="submenu-danger__item dropdown">
-
-                    <div v-if="logeado == 1">
-                        <img :src="'/img/users/'+ user.avatar" :attr="user" style="width:46px; height:46px; border-radius:50%;">
-                        <div class="dropdown-content">
-                            <div v-if="user.admin == 1">
-                                <router-link class="btn btn-default width100" to="/dashboard" tag="input" type="submit" value="Panel de control">Panel de control</router-link>
-                            </div>
-                            <router-link class="btn btn-default width100" to="/usuario" tag="input" type="submit" value="Ver usuario">Ver usuario</router-link>
-                            <div class="btn btn-default width100" @click="cerrarSesion">Cerrar sesión</div>
-                        </div>
-                    </div>
-                    <div v-else>
-                        <img src="../icons/user_icon.png" alt="">
-                        <div class="dropdown-content">
-                            <div class="btn btn-default width100" data-toggle="modal" data-target="#registrar"> Registrar</div>
-                            <div class="btn btn-default width100" data-toggle="modal" data-target="#login"> Login</div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="submenu-danger__item">
-                    <router-link to="/about">
-                        <img src="../icons/info_icon.png" alt="" style="width:50px;">
-                    </router-link>
-                </div>
-                <div class="submenu-danger__item">
-                    <carrito-desplegable></carrito-desplegable>
-                </div>
-            </div>
-        </div>
-        </transition>
-
-        <transition name="fade">
-        <div class="menu__item menu__item--collapse" v-if="visibility">
-            <div class="submenu-danger" id="submenuDanger">
-                <div class="submenu-danger__item">
-                    <div v-if="logeado">
-                        <img :src="'/img/users/'+ user.avatar" :attr="user" style="width:46px; height:46px; border-radius:50%;">
-                    </div>
-                    <div v-else>
-                        <router-link to="/login">
-                            <img src="../icons/user_icon.png" alt="">
-                        </router-link>
-                    </div>
-                </div>
-                <div class="submenu-danger__item">
-                    <router-link to="/about">
-                        <img src="../icons/info_icon.png" alt="" style="width:50px;">
-                    </router-link>
-                </div>
-                <div class="submenu-danger__item">
-                    <carrito-desplegable></carrito-desplegable>
-                </div>
-            </div>
-        </div>
-        </transition>
-        
+    <div class="menu__item menu__item--logo">
+      <router-link to="/">
+        <img src="../images/logo.png" alt="Localia Home" style="width:70px;">
+      </router-link>
     </div>
-    
+
+    <div class="menu__item menu__item--default">
+      <form class="form" v-on:submit.prevent="buscar">
+        <i class="fas fa-search"></i>
+        <input
+          type="text"
+          placeholder="Lugares.."
+          name="search"
+          maxlength="50"
+          id="ciudadBuscar"
+          class="menu__search"
+        >
+        <!-- v-model="ciudad" -->
+        <router-link
+          class="form__button"
+          to="/search"
+          tag="input"
+          type="submit"
+          value="Buscar"
+        >Buscar</router-link>
+      </form>
+    </div>
+    <div class="menu__item menu__item--content">
+      <div @click="doDropdown">
+        <img src="../icons/burger_menu.png" alt>
+      </div>
+    </div>
+
+    <transition name="fade">
+      <div class="menu__item menu__item--layout">
+        <div class="submenu-danger" id="submenuDanger">
+          <div class="submenu-danger__item dropdown">
+            <div v-if="logeado == 1">
+              <img
+                :src="'/img/users/'+ user.avatar"
+                :attr="user"
+                style="width:46px; height:46px; border-radius:50%;"
+              >
+              <div class="dropdown-content">
+                <div v-if="user.admin == 1">
+                  <router-link
+                    class="btn btn-default width100"
+                    to="/dashboard"
+                    tag="input"
+                    type="submit"
+                    value="Panel de control"
+                  >Panel de control</router-link>
+                </div>
+                <router-link
+                  class="btn btn-default width100"
+                  to="/usuario"
+                  tag="input"
+                  type="submit"
+                  value="Ver usuario"
+                >Ver usuario</router-link>
+                <div class="btn btn-default width100" @click="cerrarSesion">Cerrar sesión</div>
+              </div>
+            </div>
+            <div v-else>
+              <img src="../icons/user_icon.png" alt>
+              <div class="dropdown-content">
+                <div
+                  class="btn btn-default width100"
+                  data-toggle="modal"
+                  data-target="#registrar"
+                >Registrar</div>
+                <div class="btn btn-default width100" data-toggle="modal" data-target="#login">Login</div>
+              </div>
+            </div>
+          </div>
+          <div class="submenu-danger__item">
+            <router-link to="/about">
+              <img src="../icons/info_icon.png" alt style="width:50px;">
+            </router-link>
+          </div>
+          <div v-if="logeado == 1">
+            <div class="submenu-danger__item">
+              <carrito-desplegable></carrito-desplegable>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+
+    <transition name="fade">
+      <div class="menu__item menu__item--collapse" v-if="visibility">
+        <div class="submenu-danger" id="submenuDanger">
+          <div class="submenu-danger__item">
+            <div v-if="logeado">
+              <img
+                :src="'/img/users/'+ user.avatar"
+                :attr="user"
+                style="width:46px; height:46px; border-radius:50%;"
+              >
+            </div>
+            <div v-else>
+              <router-link to="/login">
+                <img src="../icons/user_icon.png" alt>
+              </router-link>
+            </div>
+          </div>
+          <div class="submenu-danger__item">
+            <router-link to="/about">
+              <img src="../icons/info_icon.png" alt style="width:50px;">
+            </router-link>
+          </div>
+          <div v-if="logeado == 1">
+            <div class="submenu-danger__item">
+              <carrito-desplegable></carrito-desplegable>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import CarritoDesplegable from '@/components/CarritoDesplegable.vue'
-import FormLogSign from '@/components/LoginSignUpForm.vue'
+import Vue from "vue";
+import CarritoDesplegable from "@/components/CarritoDesplegable.vue";
+import FormLogSign from "@/components/LoginSignUpForm.vue";
 
 export default Vue.extend({
-    components:{
-        CarritoDesplegable,
-        FormLogSign
+  components: {
+    CarritoDesplegable,
+    FormLogSign
+  },
+
+  methods: {
+    doDropdown() {
+      this.visibility = !this.visibility;
     },
 
-    methods:{
-        doDropdown(){
-           this.visibility = !this.visibility;
-        },
-
-        cerrarSesion(){
-
-            this.$store.dispatch('logout');
-        }
-    },
-
-    data(){
-        return {
-            visibility: true
-        }
-    },
-
-    computed:{
-
-            logeado():any{
-                return this.$store.getters.logeado;
-            },
-            user():any{
-                return this.$store.getters.getUser;
-            }
+    cerrarSesion() {
+      this.$store.dispatch("logout");
     }
-    
-})
+  },
+
+  data() {
+    return {
+      visibility: true
+    };
+  },
+
+  computed: {
+    logeado(): any {
+      return this.$store.getters.logeado;
+    },
+    user(): any {
+      return this.$store.getters.getUser;
+    }
+  }
+});
 </script>
 
 
 <style lang="scss">
-
- @import "../styles/style.scss";
-
+@import "../styles/style.scss";
 </style>
